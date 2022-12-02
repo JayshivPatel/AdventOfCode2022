d01e3b74c7fff23675d3dd66593c50611db235a8
jdp221 <jdp221@ic.ac.uk>
2022-12-02 16:43:41 +0000

    Day 2 Part 2 finished

--- Day 2/Main.scala
+++ Day 2/Main.scala
@@ -15,13 +15,10 @@ object Main {
   }
 
   def calculateScore(args: Array[String]): Int = {
-    args.map(x => {
-      if (x.length == 3)
-        pointsGained(x.charAt(0), x.charAt(2)) else 0
-    }).sum
+    args.map(x => {pointsGainedTwo(x.charAt(0), x.charAt(2))}).sum
   }
 
-  def pointsGained(opponent: Char, you: Char): Int = {
+  def pointsGainedOne(opponent: Char, you: Char): Int = {
     opponent match {
       case 'A' =>
         you match {
@@ -49,6 +46,43 @@ object Main {
             Main.PAPER + Main.LOSS
           case 'Z' =>
             Main.SCISSORS + Main.DRAW
+        }
+    }
+  }
+
+  def pointsGainedTwo(opponent: Char, you: Char): Int = {
+    opponent match {
+      //ROCK
+      case 'A' =>
+        you match {
+          case 'X' =>
+            Main.SCISSORS + Main.LOSS
+          case 'Y' =>
+            Main.ROCK + Main.DRAW
+          case 'Z' =>
+            Main.PAPER + Main.WIN
+        }
+
+      //PAPER
+      case 'B' =>
+        you match {
+          case 'X' =>
+            Main.ROCK + Main.LOSS
+          case 'Y' =>
+            Main.PAPER + Main.DRAW
+          case 'Z' =>
+            Main.SCISSORS + Main.WIN
+        }
+
+      //SCISSORS
+      case 'C' =>
+        you match {
+          case 'X' =>
+            Main.PAPER + Main.LOSS
+          case 'Y' =>
+            Main.SCISSORS + Main.DRAW
+          case 'Z' =>
+            Main.ROCK + Main.WIN
         }
     }
   }
