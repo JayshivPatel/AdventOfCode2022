c222efe8146c2c3c8d65a9aa5a724594f829bd07
jdp221 <jdp221@ic.ac.uk>
2022-12-02 16:25:07 +0000

    Day 1 part 2 finished

--- Day 1/Main.scala
+++ Day 1/Main.scala
@@ -1,9 +1,15 @@
 import scala.io.Source
+import scala.util.control.Breaks._
 
 object Main {
   def main(args: Array[String]): Unit = {
     val buffer = Source.fromFile("Numbers.txt")
-    print(findMax(buffer.getLines.toArray))
+    val array = findTopThree(buffer.getLines.toArray)
+    var sum = 0
+    for (item <- array) {
+      sum += item
+    }
+    print(sum)
     buffer.close()
   }
 
@@ -17,10 +23,38 @@ object Main {
         }
         temp = 0
       } else {
-        var toAdd = line.toInt
-        temp += toAdd
+        temp += line.toInt
       }
     }
     return max
+  }
+
+  def findTopThree(lines: Array[String]): List[Int] = {
+    var max1 = 0
+    var max2 = 0
+    var max3 = 0
+    var temp = 0
+
+    for (line <- lines) {
+      if (line.isBlank) {
+        if (temp > max1) {
+          val copy = max2
+          max3 = copy
+          max2 = max1
+          max1 = temp
+        } else if (temp > max2) {
+          val copy = max2
+          max3 = max2
+          max2 = temp
+        } else if (temp > max3) {
+          max3 = temp
+        }
+        temp = 0
+      }
+      else {
+        temp += line.toInt
+      }
+    }
+    return List(max1, max2, max3)
   }
 }
